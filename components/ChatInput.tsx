47e2a1bc36ffd05a2eccd7166ce8c8c5ac25e902
Micheal <khanzaidan786@gmail.com>
2024-04-16 03:15:33 +0530

    send message functionality init

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,10 +1,21 @@
+"use client";
 import React from "react";
 import { Input } from "./ui/input";
 
 function ChatInput() {
+  const handleSendMessage = (text: string) => {
+    alert(text);
+  };
   return (
     <div className="p-5 ">
-      <Input placeholder="send message" />
+      <Input
+        placeholder="send message"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSendMessage(e.currentTarget.value);
+          }
+        }}
+      />
     </div>
   );
 }
